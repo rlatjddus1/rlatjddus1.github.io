f4557bbad4353381382ff5249052d74318a8491d
rlatjddus1 <rlatjduds234@gmail.com>
2026-09-19 17:09:54 +0900

    Fix project rendering

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -49,11 +49,13 @@ export default function Projects() {
           <h1 className="project-title">Open Source Projects</h1>
           <div className="repo-cards-div-main">
             {repo.map((v, i) => {
-              if (!v) {
+              if (!v || !v.node) {
                 console.error(
                   `Github Object for repository number : ${i} is undefined`
                 );
+                return null;
               }
+
               return (
                 <GithubRepoCard repo={v} key={v.node.id} isDark={isDark} />
               );
